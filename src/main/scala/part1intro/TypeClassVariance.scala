5b7e2a52ca0dc0bbbcbf47db20d3e53c84688bea
core <cornet.remi@gmail.com>
2024-03-31 09:08:53 -0400

    Type Classes Variance/Covariance/Contravariance

--- src/main/scala/part1intro/TypeClassVariance.scala
+++ src/main/scala/part1intro/TypeClassVariance.scala
@@ -13,6 +13,10 @@ object TypeClassVariance {
   class Animal
   class Cat extends Animal
 
+  // Rule of thumb:
+  // "HAS a T" = covariant
+  // "ACTS on T" = contravariant
+
   // Covariant type
   class Cage[+T]
   val cage: Cage[Animal] = new Cage[Cat]
@@ -20,4 +24,31 @@ object TypeClassVariance {
   // Contravariant type
   class Vet[-T]
   val vet: Vet[Cat] = new Vet[Animal]
+
+  trait SoundMaker[-T]
+  implicit object AnimalSoundMaker extends SoundMaker[Animal]
+
+  def makeSound[T](implicit soundMaker: SoundMaker[T]): Unit = println("Wow")
+  makeSound[Animal] // Ok, TC instances defined above
+  makeSound[Cat] // Ok, TC instance for Animal is also applicable to Cats
+
+  implicit object OptionSoundMaker extends SoundMaker[Option[Int]]
+  makeSound[Option[Int]]
+  makeSound[Some[Int]]
+
+  trait AnimalShow[+T] {
+    def show: String
+  }
+  implicit object GeneralAnimalShow extends AnimalShow[Animal] {
+    override def show: String = "Animals everywhere"
+  }
+  implicit object CatsShow extends AnimalShow[Cat] {
+    override def show: String = "So many cats"
+  }
+  def organizeShow[T](implicit event: AnimalShow[T]): String = event.show
+
+  def main(args: Array[String]): Unit = {
+    println(organizeShow[Cat])
+    // println(organizeShow[Animal]) // Compilation KO, ambiguous values
+  }
 }
